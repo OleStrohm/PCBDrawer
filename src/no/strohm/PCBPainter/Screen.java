181702c0e7c37bd5dc4124a6f3a45c04abd766b8
Anonypu5 <elias.fyksen@grabit.no>
2017-04-14 16:56:48 +0100

    Tets

--- src/no/strohm/PCBPainter/Screen.java
+++ src/no/strohm/PCBPainter/Screen.java
@@ -9,62 +9,8 @@ import java.awt.image.DataBufferInt;
 /**
  * Created by Ole on 13.04.2017.
  */
-public class Screen extends Canvas {
+public class Screen extends Component {
 
-	private JFrame frame;
 
-	private boolean initialized = false;
-
-	private BufferedImage image;
-	private int[] pixels;
-	private int width, height;
-
-	public Screen(int width, int height) {
-		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-		this.width = width;
-		this.height = height;
-
-		init();
-	}
-
-	private void init() {
-		if(initialized)
-			return;
-
-		Dimension d = new Dimension(width, height);
-		setPreferredSize(d);
-		setMaximumSize(d);
-		setMinimumSize(d);
-
-		frame = new JFrame("PCB Creator");
-
-		initialized = true;
-	}
-
-	public void render() {
-		BufferStrategy bs = getBufferStrategy();
-		if(bs == null) {
-			createBufferStrategy(2);
-			return;
-		}
-
-		Graphics g = bs.getDrawGraphics();
-		g.drawImage(image, 0, 0, width, height, null);
-		g.dispose();
-		bs.show();
-	}
-
-	public int[] getPixels() {
-		return pixels;
-	}
-
-	public int getWidth() {
-		return width;
-	}
-
-	public int getHeight() {
-		return height;
-	}
 
 }
