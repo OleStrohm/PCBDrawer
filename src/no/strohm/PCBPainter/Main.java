2e16620c6ecf1b4d16e160ef54e15696d592015c
Ole Strohm <strohm99@gmail.com>
2017-04-14 19:27:58 +0200

    fiksa en liten error

--- src/no/strohm/PCBPainter/Main.java
+++ src/no/strohm/PCBPainter/Main.java
@@ -1,18 +1,21 @@
 package no.strohm.PCBPainter;
 
-import no.strohm.PCBPainter.graphics.Screen;
+import no.strohm.PCBPainter.graphics.Window;
 
 /**
  * Created by Ole on 13.04.2017.
  */
 public class Main implements Runnable {
 
-
-
+	private Window window;
 	private boolean running = true;
+	
+	public Main() {
+		window = new Window("Let's make a PCB", 800, 600);
+	}
 
 	public void run() {
-		new Screen();
+
 
 		while(running) {
 
