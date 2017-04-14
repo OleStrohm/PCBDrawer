afa525b07eb26d3cd606062ee1acfbf5bbf5d9fd
Ole Strohm <strohm99@gmail.com>
2017-04-14 17:23:41 +0200

    setup

---
+++ src/no/strohm/PCBPainter/Main.java
@@ -0,0 +1,32 @@
+package no.strohm.PCBPainter;
+
+/**
+ * Created by Ole on 13.04.2017.
+ */
+public class Main implements Runnable {
+
+	private boolean running = true;
+
+	public void run() {
+
+
+		while(running) {
+
+		}
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+
+		main.start();
+	}
+
+	private void start() {
+		new Thread(this).start();
+	}
+
+	public void stop() {
+		running = false;
+	}
+
+}
