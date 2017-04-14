a092e9ce0d363920964ac298563d1111ace00e50
Anonypu5 <elias.fyksen@grabit.no>
2017-04-14 18:16:07 +0100

    lkdfjlsaj

--- src/no/strohm/PCBPainter/Image.java
+++ src/no/strohm/PCBPainter/Image.java
@@ -1,6 +1,8 @@
 package no.strohm.PCBPainter;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import java.nio.Buffer;
 
 /**
@@ -24,7 +26,26 @@ public class Image {
     }
 
     public void init(){
-        img = img;
+        try{
+            BufferedImage temp = ImageIO.read(file);
+            img = new BufferedImage(temp.getWidth(), temp.getHeight(), BufferedImage.TYPE_INT_RGB);
+            img.getGraphics().drawImage(temp,0,0,null);
+            pixels = ((DataBufferInt)img.getRaster().getDataBuffer()).getData();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
+    public int getPixel(int x, int y){
+        if(x < 0 || y < 0 || x > getWidth() || y > getHeight()) return 0;
+        return pixels[x + y * getWidth()];
+    }
+
+    public int getWidth(){
+        return img.getWidth();
+    }
+
+    public int getHeight(){
+        return img.getHeight();
+    }
 }
