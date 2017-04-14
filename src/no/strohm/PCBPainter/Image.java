d8e42fac9cdae1fb0671b30f54483e049a2d4956
Anonypu5 <elias.fyksen@grabit.no>
2017-04-14 18:00:48 +0100

    dfsa

--- src/no/strohm/PCBPainter/Image.java
+++ src/no/strohm/PCBPainter/Image.java
@@ -1,6 +1,7 @@
 package no.strohm.PCBPainter;
 
 import java.awt.image.BufferedImage;
+import java.nio.Buffer;
 
 /**
  * Created by Elias F. Fyksen on 14.04.2017.
@@ -14,14 +15,16 @@ public class Image {
 
     public Image(File m_file){
         file = m_file;
+        init();
     }
 
     public Image(String path){
         file = new File(path);
+        init();
     }
 
     public void init(){
-        init();
+        img = img;
     }
 
 }
